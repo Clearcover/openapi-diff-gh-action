c2c9a5bfc70762ec9384df25261ab71825f5462d
Naresh Baliga <nbaliga@clearcover.com>
2021-09-30 16:20:11 -0500

    Switch order.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 service_name=$1
 new_spec=$2
 current_spec=$3
-html_file="$service_name"-openapi-diff-results.html
+html_file="$service_name"-openapi-diff.html
 
-echo "Service name = $service_name , new_spec = $new_spec , current_spec = $current_spec, html_file = $html_file"
+echo "::info ::service_name = $service_name , new_spec = $new_spec , current_spec = $current_spec, html_file = $html_file"
 
-state=$(/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --state "$new_spec" "$current_spec" 2>&1)
+state=$(/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --state "$current_spec" "$new_spec" 2>&1)
 
 echo "::set-output name=openapi-diff-state::$state"
 
@@ -15,6 +15,6 @@ then
   echo "::warning ::Breaking/Incompatible OpenAPI changes found for $service_name!!"
 fi
 
-/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --html "$html_file" "$new_spec" "$current_spec"
+/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --html "$html_file" "$current_spec" "$new_spec"
 
 echo "::set-output name=openapi-html-result::$html_file"
