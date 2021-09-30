eebab9d7cb65221ccdb993c319aa6d24cf3ad59e
Naresh Baliga <nbaliga@clearcover.com>
2021-09-30 11:53:25 -0500

    Switch for running state of diff only.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-echo "Running openapi-diff with $1 and $2 args"
-result=$(/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar $1 $2 2>&1)
-echo $result
+echo "Running openapi-diff with $1,$2,$3 args"
+state_only=$3
+
+if [ "$state_only" = 'true' ]
+then
+  result=$(/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --state "$1" "$2" 2>&1)
+fi
+
+echo "$result"
 echo "::set-output name=openapi-diff-result::$result"
