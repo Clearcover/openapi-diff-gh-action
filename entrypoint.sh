115a408482adae63615a28ad4cd7dccd27483a52
Naresh Baliga <nbaliga@clearcover.com>
2021-09-30 15:38:24 -0500

    Use service name to refer to outputs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/bash -l
+service_name=$1
+new_spec=$2
+current_spec=$3
+html_file="$service_name"-openapi-diff-results.html
 
-state=$(/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --state "$1" "$2" 2>&1)
+state=$(/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --state "$new_spec" "$current_spec" 2>&1)
 
 echo "::set-output name=openapi-diff-state::$state"
 
 if [ "$state" = 'incompatible' ]
 then
-  echo "::warning ::Breaking/Incompatible OpenAPI changes found!!"
+  echo "::warning ::Breaking/Incompatible OpenAPI changes found for $service_name!!"
 fi
 
-/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --html openapi-diff-results.html "$1" "$2"
+/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --html "$html_file" "$new_spec" "$current_spec"
 
-echo "::set-output name=openapi-html-result::openapi-diff-results.html"
+echo "::set-output name=openapi-html-result::$html_file"
