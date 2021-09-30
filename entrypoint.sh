e83c62398fe9b158daf8b946161ef3a4ac6b03db
Naresh Baliga <nbaliga@clearcover.com>
2021-09-30 11:29:20 -0500

    Fix Java not found issue.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 echo "Running openapi-diff with $1 and $2 args"
-result=$(java -jar /app/openapi-diff.jar $1 $2 2>&1)
+result=$(/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar $1 $2 2>&1)
 echo $result
 echo "::set-output name=openapi-diff-result::$result"
