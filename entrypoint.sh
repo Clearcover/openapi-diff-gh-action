e4cbd1c1797c12769113f3cb409f8096e3b1220f
Naresh Baliga <nbaliga@clearcover.com>
2021-09-30 14:42:25 -0500

    Output HTML file result.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -l
 
-echo "Running openapi-diff with $1,$2,$3 args"
-
 state=$(/usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --state "$1" "$2" 2>&1)
 
-echo "$state"
 echo "::set-output name=openapi-diff-state::$state"
 
 if [ "$state" = 'incompatible' ]
@@ -12,6 +9,6 @@ then
   echo "::warning ::Breaking/Incompatible OpenAPI changes found!!"
 fi
 
-pwd
 /usr/local/openjdk-8/bin/java -jar /app/openapi-diff.jar --fail-on-incompatible --html openapi-diff-results.html "$1" "$2"
-ls -l *.html
+
+echo "::set-output name=openapi-html-result::openapi-diff-results.html"
